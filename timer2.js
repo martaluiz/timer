3e26ece546de3b0f085a82bcc4223a216f30cb73
Marta Luiz Avelino <martaluizavelino@gmail.com>
2020-02-12 00:21:15 +0000

    Interactive Timer

---
+++ timer2.js
@@ -0,0 +1,30 @@
+
+// The user can press b and it should beep right away
+// The user can input any number from 1 to 9 and it should
+// immediately output "setting timer for x seconds...", and
+// beep after that number of seconds has passed
+// The user can use ctrl + c to exit the program, at which point the program should say "Thanks for using me, ciao!" before terminating
+
+
+const stdin = process.stdin;
+// don't worry about these next two lines of setup work.
+stdin.setRawMode(true);
+stdin.setEncoding('utf8');
+
+stdin.on('data', (key) => {
+  process.stdout.write(key);
+  if (key === 'b') {
+    process.stdout.write('\x07');
+  }
+  if (Number(key) > 0 && Number(key) !== NaN ) {
+    process.stdout.write(`setting timer for ${key} seconds..`)
+    setTimeout(() => {
+    process.stdout.write('\x07');
+    }, Number(key) * 1000);
+}
+  if (key === '\u0003') {
+    console.log('Thanks for using me, ciao');
+    process.exit();
+  }
+});
+
